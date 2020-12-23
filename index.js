f18b42a6abe149050cd43d10411b7f7562704ccc
John Dust <d24dust@hotmail.com>
2020-12-23 23:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function distanceFromHqInBlocks(blocks) {
   return (42 > blocks) ? 42 - blocks : blocks - 42
 }
 
-function distanceFromHqInFeet(feet) {
-  
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264
 }
